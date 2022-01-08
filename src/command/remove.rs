15c7b91e690d9f0a97f7a18a9eaa36999367ae7d
Its <2779546+Its-its@users.noreply.github.com>
2022-01-08 00:54:48 -0800

    Dont remove bridge if it contains other hosts

--- src/command/remove.rs
+++ src/command/remove.rs
@@ -23,6 +23,14 @@ pub fn process(addr_or_host: &str, netsh: &mut NetSH, hosts: &mut HostFile) -> R
 		let removed = hosts.delete(DeletionType::Host(addr_or_host))?;
 
 		for host in removed {
+			// Check to see if we have others in the bridge.
+			if let Some(bridge) = netsh.get_by_connection((host.address, 80).into()) {
+				if hosts.count_addresses(bridge.listen_to.address) != 0 {
+					continue;
+				}
+			}
+
+			// Delete Bridge if this was the only item in the bridge.
 			netsh.delete((host.address, 80).into(), ProxyBridge::V4ToV4)?;
 		}
 	}
